e34c4c0b73e95ffde332f4dddb0a401a744f6de1
Momina Din <momina@mominas-mbp-2.mynetworksettings.com>
2024-08-13 18:41:21 -0400

    updated index.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // import the pets array from data.js
 const pets = require('./data');
+pets.forEach(pet => {
+    console.log(`Pet Name: ${pet.name}, Owner: ${pet.owner}`);
+});
 
 // init express app
 const express = require('express');
@@ -10,7 +13,7 @@ const PORT = 8080;
 // GET - / - returns homepage
 app.get('/', (req, res) => {
     // serve up the public folder as static index.html file
-
+    app.use(express.static('public'));
 });
 
 // hello world route
@@ -21,31 +24,31 @@ app.get('/api', (req, res) => {
 // get all pets from the database
 app.get('/api/v1/pets', (req, res) => {
     // send the pets array as a response
-
+    res.json(pets);
 });
 
 // get pet by owner with query string
 app.get('/api/v1/pets/owner', (req, res) => {
     // get the owner from the request
-
+    const owner = req.query.owner;
 
     // find the pet in the pets array
     const pet = pets.find(pet => pet.owner === owner);
 
     // send the pet as a response
-
+    res.json(pet);
 });
 
 // get pet by name
 app.get('/api/v1/pets/:name', (req, res) => {
     // get the name from the request
-
+    const { name } = req.params;
 
     // find the pet in the pets array
     const pet = pets.find(pet => pet.name === name);
 
     // send the pet as a response
-
+    res.json(pet);
 });
 
 app.listen(PORT, () => {
